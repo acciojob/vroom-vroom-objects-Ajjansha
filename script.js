b9b38b199dcacf672d1e1ef9f45f9ad26f612e4d
Ajjansha <141652880+Ajjansha@users.noreply.github.com>
2026-02-06 18:23:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Method on Car prototype
+Car.prototype.getMakeModel = function () {
+    return this.make + " " + this.model;
+};
 
-// Do not change the code below
+function SportsCar(make, model, topSpeed) {
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
+}
+
+// Inherit from Car prototype
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// Method on SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+    return this.topSpeed;
+};
+
+// Demo usage
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+
+document.getElementById("btn").addEventListener("click", function () {
+    document.getElementById("makeModel").textContent =
+        "Car: " + car.getMakeModel();
+
+    document.getElementById("topSpeed").textContent =
+        "Top Speed: " + car.getTopSpeed() + " mph";
+});
+
+// Do not change
 window.Car = Car;
 window.SportsCar = SportsCar;
